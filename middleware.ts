2bd82929d5f995e4cd0aa15ccc2088a89e2d000d
Bas3L3ss <phamthen.hun060907@gmail.com>
2024-07-07 15:16:07 +0700

    commit

--- middleware.ts
+++ middleware.ts
@@ -14,3 +14,7 @@ export default clerkMiddleware(async (auth, req) => {
   }
   if (!isPublicRoute(req)) auth().protect();
 });
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
